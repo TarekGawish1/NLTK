7a369e1c4498767f475f9f82f009246c0ecd51c5
TarekGawish1 <tarek29gawesh55@gmail.com>
2024-05-17 20:59:11 +0300

    n-grams in NLTK

--- sec3.py
+++ sec3.py
@@ -2,4 +2,9 @@ import nltk
 
 from nltk import ngrams
 
-sentence = input("The sun is shining brightly.")
+sentence =("The sun is shining brightly.")
+n = int(input("Enter the value of n: " ))
+
+n_grams = ngrams(sentence.split(),n)
+for grams in n_grams:
+    print(grams)
