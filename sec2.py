eac9e3405b85081556dd358cf1222583e447567c
TarekGawish1 <tarek29gawesh55@gmail.com>
2024-05-18 06:23:46 +0300

    snow-ball

--- sec2.py
+++ sec2.py
@@ -1,13 +1,13 @@
 # stem porter
 from nltk.stem.porter import PorterStemmer as p_stem
 
-s_stem_ENG = s_stem(language="english")
+
 print(p_stem().stem("dogs"))
 
 
 # snowball
 from nltk.stem.snowball import SnowballStemmer as s_stem
-
-w_plu = ['doors', 'dogs', 'photos', 'coputer', 'communications']
+s_stem_ENG = s_stem(language="english")
+w_plu = ['doors', 'dogs', 'photos', 'computer', 'communications']
 sgls = [s_stem_ENG.stem(wplu) for wplu in w_plu]
 print(" ".join(sgls))
